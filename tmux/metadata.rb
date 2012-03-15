1254c0586055558a80852ee70ad4f234ffd8b1f5
Olivier Melcher <olivier.melcher@gmail.com>
2012-03-15 20:37:30 +0000

    bump tmux version

--- tmux/metadata.rb
+++ tmux/metadata.rb
@@ -3,4 +3,4 @@ maintainer_email "olivier.melcher@gmail.com"
 license          "MIT"
 description      "Installs/Configures tmux"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.6"
+version          "0.0.7"
